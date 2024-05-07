f3cf9c4de2b610d0e17be8588dfa5e058e82e18e
Tan ChongZhi <tanchongzhi@outlook.com>
2024-05-07 13:16:41 +0800

    Update Windows Defender protection history cleaner for Windows 7 later.

--- Cleaners/Windows.Defender.ps1
+++ Cleaners/Windows.Defender.ps1
@@ -2,11 +2,26 @@ $ErrorActionPreference = "SilentlyContinue"
 
 Write-Host "Cleaning Windows Defender History..."
 
-$Path = "$env:ProgramData\Microsoft\Windows Defender\Scans\History\*"
-Remove-Item -Path $Path -Recurse -Force
+if ([System.Environment]::OSVersion.Version.Major -le 6) {
+    $Path = "$env:ProgramData\Microsoft\Windows Defender\Scans\History\*"
+    Remove-Item -Path $Path -Recurse -Force
 
-$Path = "$env:ProgramData\Microsoft\Windows Defender\Support\*"
-$Includes  = @(
-    "*.log"
-)
-Get-ChildItem -Path $Path -Include $Includes  -Recurse -Force | Where-Object { -not $_.PSIsContainer } | Remove-Item -Force
+    $Path = "$env:ProgramData\Microsoft\Windows Defender\Support\*"
+    $Includes  = @(
+        "*.log"
+    )
+    Get-ChildItem -Path $Path -Include $Includes  -Recurse -Force | Where-Object { -not $_.PSIsContainer } | Remove-Item -Force
+} else {
+    $FolderPath = 'C:\ProgramData\Microsoft\Windows Defender\Scans\History\Service'
+    $FilePath = "$FolderPath\Detections.log"
+
+    if (Test-Path -Path $FilePath) {
+        Write-Host "    Restart required."
+
+        $null = schtasks /query /tn ClearWinDefendHistoryOnStartupOnce 2>&1
+
+        if (-not $?) {
+            schtasks /create /f /sc onStart /ru 'NT AUTHORITY\SYSTEM' /tn ClearWinDefendHistoryOnStartupOnce /tr "cmd.exe /c rd /s /q \`"$FolderPath\`" & schtasks /delete /f /tn ClearWinDefendHistoryOnStartupOnce"
+        }
+    }
+}
