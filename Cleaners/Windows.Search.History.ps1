16c02b63c81acc6887f4499059ae2e5062e2e94f
Tan ChongZhi <tanchongzhi@outlook.com>
2024-06-15 08:16:11 +0800

    Update Windows Search cleaner.

--- Cleaners/Windows.Search.History.ps1
+++ Cleaners/Windows.Search.History.ps1
@@ -2,9 +2,22 @@ $ErrorActionPreference = "SilentlyContinue"
 
 Write-Host "Cleaning Windows Search History..."
 
+$serviceStatus = (Get-Service -Name WSearch).Status
+
+if ($serviceStatus -eq "Running") {
+    Set-Service -Name WSearch -Status Stopped
+}
+
 $Path = "$env:ProgramData\Microsoft\Search\*"
 $Includes  = "*.log"
 Get-ChildItem -Path $Path -Include $Includes  -Recurse -Force | Where-Object { -not $_.PSIsContainer } | Remove-Item -Force
 
+$Path = "$env:ProgramData\Microsoft\Search\Data\Applications\Windows\Windows.edb"
+Remove-Item -Path $Path -Recurse -Force
+
 $Path = "$env:LOCALAPPDATA\Microsoft\Windows\ConnectedSearch\History\*"
 Remove-Item -Path $Path -Recurse -Force
+
+if ($serviceStatus -eq "Running") {
+    Set-Service -Name WSearch -Status Running
+}
